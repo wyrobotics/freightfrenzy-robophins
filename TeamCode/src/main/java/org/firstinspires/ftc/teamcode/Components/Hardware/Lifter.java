f340eb8451f8fe027205078691b82dc0fc993717
gumbymoo <julian.b.shah@gmail.com>
2021-12-14 20:21:06 -0600

    Drive Constants tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Hardware/Lifter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Hardware/Lifter.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Components.Hardware;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -11,6 +12,9 @@ public class Lifter {
     DcMotor lifter;
     Servo slapper;
 
+    TouchSensor upSwitch;
+    TouchSensor downSwitch;
+
     public Lifter(HardwareMap hardwareMap, Telemetry telemetry) {
 
         lifter = hardwareMap.get(DcMotor.class, "lifter");
@@ -18,12 +22,15 @@ public class Lifter {
 
         slapper = hardwareMap.get(Servo.class, "slapper");
 
+        upSwitch = hardwareMap.get(TouchSensor.class, "upSwitch");
+        downSwitch = hardwareMap.get(TouchSensor.class, "downSwitch");
+
     }
 
     public void moveLifter(double power) { lifter.setPower(power); }
 
-    public void lift() { moveLifter(0.5); }
-    public void drop() { moveLifter(-0.5); }
+    public void lift() { moveLifter(upSwitch.isPressed() ? 0 : 0.5); }
+    public void drop() { moveLifter(downSwitch.isPressed() ? 0 : -0.5); }
     public void stop() { moveLifter(0); }
 
     public void slap() { slapper.setPosition(0.8); }
